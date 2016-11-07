ea61f6fff5e8977018ff1c6b1959fb74fd7bbb0c
hama_du <dercll1020@gmail.com>
2016-11-08 08:04:44 +0900

    refine installation script

--- install.sh
+++ install.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
+echo "=================================================="
+echo " =====                                      ====="
+echo " = 💻   Set-up macos development environment  💻  ="
+echo " =====                                      ====="
+echo "=================================================="
+
 # defaults
+echo "[💫 ] changing macos default setting"
 sh "$PWD/macos/defaults.sh"
 
 # homebrew
+echo "[💫 ] installing brew packages"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap Homebrew/bundle
 brew bundle --file="$PWD/macos/homebrew/Brewfile"
 
-# chsh
+# zsh
+echo "[💫 ] setting zsh"
 chsh -s /usr/local/bin/zsh
 ln -Fis "$PWD/.zshenv" $HOME
 ln -Fis "$PWD/.zshrc" $HOME
